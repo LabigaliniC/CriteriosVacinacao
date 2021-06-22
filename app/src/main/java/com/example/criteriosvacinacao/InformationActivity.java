94b82b22fdfd664bbc840005abb32d5e8e5b36ca
Carlos <carlosaugusto_26@hotmail.com>
2021-06-22 20:52:05 -0300

    Second version

--- app/src/main/java/com/example/criteriosvacinacao/InformationActivity.java
+++ app/src/main/java/com/example/criteriosvacinacao/InformationActivity.java
@@ -12,20 +12,20 @@ public class InformationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information);
 
-        TextView ageTextView = findViewById(R.id.show_name);
-        TextView scoreTextView = findViewById(R.id.age_value);
-        TextView mortalityTextView = findViewById(R.id.email_value);
+        TextView showNameLabel = findViewById(R.id.show_name);
+        //TextView age_Value = findViewById(R.id.age_value);
+        TextView email_Value = findViewById(R.id.email_value);
         TextView statusTextView = findViewById(R.id.prevision_value);
 
         Intent intent = getIntent();
-        int age = intent.getIntExtra(MainActivity.EXTRA_AGE, 0);
-        int score = intent.getIntExtra(MainActivity.EXTRA_SCORE, 0);
-        int mortality = intent.getIntExtra(MainActivity.EXTRA_MORTALITY, 0);
-        boolean serious = intent.getBooleanExtra(MainActivity.EXTRA_SERIOUS, false);
+        int name = intent.getIntExtra(MainActivity.EXTRA_NAME, 0);
+        int email = intent.getIntExtra(MainActivity.EXTRA_EMAIL, 0);
+        int priority = intent.getIntExtra(MainActivity.EXTRA_PRIORITY, 0);
 
-        ageTextView.setText(Integer.toString(age));
-        scoreTextView.setText(Integer.toString(score));
-        mortalityTextView.setText(Integer.toString(mortality));
-        statusTextView.setText(serious ? "Pancreatite grave" : "Pancreatite não grave");
+
+        showNameLabel.setText(Integer.toString(name));
+        email_Value.setText(Integer.toString(email));
+        statusTextView.setText(Integer.toString(priority));
+
     }
 }
--- app/src/main/java/com/example/criteriosvacinacao/MainActivity.java
+++ app/src/main/java/com/example/criteriosvacinacao/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.criteriosvacinacao;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -12,10 +13,9 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener  {
-    public static final String EXTRA_MORTALITY = "com.example.criteriosvacinacao.mortality";
-    public static final String EXTRA_AGE = "com.example.criteriosvacinacao.age";
-    public static final String EXTRA_SCORE = "com.example.criteriosvacinacao.score";
-    public static final String EXTRA_SERIOUS = "com.example.criteriosvacinacao.serious";
+    public static final String EXTRA_NAME = "com.example.criteriosvacinacao.name";
+    public static final String EXTRA_EMAIL = "com.example.criteriosvacinacao.email";
+    public static final String EXTRA_PRIORITY = "com.example.criteriosvacinacao.priority";
     private static final String TAG = "MainActivity";
     private Spinner spinnerLista;
 
@@ -42,20 +42,13 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     // launchSecondActivity
     public void calculatePrevisaoVacina(View view) {
+        String name = nameEditText.getText().toString();
+        String email = emailEditText.getText().toString();
 
         Log.d(TAG, "calcularPrevisaoVacinacao: " + spinnerLista.getSelectedItem().toString());
         //Log.d(TAG, "calcularPrevisaoVacinacao: " + spinnerLista.getItemAtPosition(1).toString());
 
         String priority;
-
-        //String nome = "Vitor";
-        //switch (nome.hashcode()) {
-        //    case "Vitor".hashCode() : System.out.println("Valor de nome é Vitor");
-        //        break;
-        //    case "Paulo".hashCode() : System.out.println("Valor de nome é Paulo");
-        //        break;
-        //    default: System.out.println("Default");
-        //}
 
         if (spinnerLista.getItemAtPosition(1) == "Idade superior a 80 anos"){
             priority = "Prioridade 1 - Data para iniciar a Vacinação dia 01/04/2021 à 01/05/2021 ";
@@ -75,55 +68,51 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         else if (spinnerLista.getItemAtPosition(6) == "Idade superior a 60 anos"){
             priority = "Prioridade 6 - Data para iniciar a Vacinação dia 01/09/2021 à 01/10/2021 ";
         }
-        else if (spinnerLista.getItemAtPosition(7) == ""){
+        else if (spinnerLista.getItemAtPosition(7) == "Com Comorbidade Comprovada"){
             priority = "Prioridade 7 - Data para iniciar a Vacinação dia 01/10/2021 à 01/11/2021 ";
         }
-        else if (spinnerLista.getItemAtPosition(8) == ""){
+        else if (spinnerLista.getItemAtPosition(8) == "Profissionais Segurança"){
             priority = "Prioridade 8 - Data para iniciar a Vacinação dia 01/11/2021 à 01/12/2021 ";
         }
-        else if (spinnerLista.getItemAtPosition(9) == ""){
+        else if (spinnerLista.getItemAtPosition(9) == "Profissionais Área Educação"){
             priority = "Prioridade 9 - Data para iniciar a Vacinação dia 01/12/2021 à 01/01/2022 ";
         }
-        else if (spinnerLista.getItemAtPosition(10) == ""){
+        else if (spinnerLista.getItemAtPosition(10) == "Motoristas Transporte Coletivo"){
             priority = "Prioridade 10 - Data para iniciar a Vacinação dia 01/01/2022 à 01/02/2022 ";
         }
-        else if (spinnerLista.getItemAtPosition(11) == ""){
+        else if (spinnerLista.getItemAtPosition(11) == "Motoristas Caminhão"){
             priority = "Prioridade 11 - Data para iniciar a Vacinação dia 01/02/2022 à 01/03/2022 ";
         }
-        else if (spinnerLista.getItemAtPosition(12) == ""){
+        else if (spinnerLista.getItemAtPosition(12) == "Profissional Atendimento ao Público"){
             priority = "Prioridade 12 - Data para iniciar a Vacinação dia 01/03/2022 à 01/04/2022 ";
         }
-        else if (spinnerLista.getItemAtPosition(13) == ""){
+        else if (spinnerLista.getItemAtPosition(13) == "Idade superior a 50 anos"){
             priority = "Prioridade 13 - Data para iniciar a Vacinação dia 01/04/2022 à 01/05/2022 ";
         }
-        else if (spinnerLista.getItemAtPosition(14) == ""){
+        else if (spinnerLista.getItemAtPosition(14) == "Grávida"){
             priority = "Prioridade 14 - Data para iniciar a Vacinação dia 01/05/2022 à 01/06/2022 ";
         }
-        else if (spinnerLista.getItemAtPosition(15) == ""){
+        else if (spinnerLista.getItemAtPosition(15) == "Idade superior a 40 anos"){
             priority = "Prioridade 15 - Data para iniciar a Vacinação dia 01/06/2022 à 01/07/2022 ";
         }
-        else if (spinnerLista.getItemAtPosition(16) == ""){
+        else if (spinnerLista.getItemAtPosition(16) == "Presidiário"){
             priority = "Prioridade 16 - Data para iniciar a Vacinação dia 01/07/2022 à 01/08/2022 ";
         }
-        else if (spinnerLista.getItemAtPosition(17) == ""){
+        else if (spinnerLista.getItemAtPosition(17) == "Idade superior a 30 anos"){
             priority = "Prioridade 17 - Data para iniciar a Vacinação dia 01/08/2022 à 01/09/2022 ";
         }
-        else if (spinnerLista.getItemAtPosition(18) == ""){
+        else if (spinnerLista.getItemAtPosition(18) == "Idade superior a 25 anos"){
             priority = "Prioridade 18 - Data para iniciar a Vacinação dia 01/09/2022 à 01/10/2022 ";
         }
-        else if (spinnerLista.getItemAtPosition(19) == ""){
+        else if (spinnerLista.getItemAtPosition(19) == "Idade superior a 18 anos"){
             priority = "Prioridade 19 - Data para iniciar a Vacinação dia 01/10/2022 à 01/11/2022 ";
-        }*/
+        }
 
-        //Intent intent = new Intent(this, InformationActivity.class);
-        //intent.putExtra(EXTRA_AGE, age);
-        //intent.putExtra(EXTRA_MORTALITY, mortality);
-        //intent.putExtra(EXTRA_AGE, age);
-        //intent.putExtra(EXTRA_SCORE, score);
-        //intent.putExtra(EXTRA_SERIOUS, serious);
-       // startActivity(intent);
-
-
+        Intent intent = new Intent(this, InformationActivity.class);
+        intent.putExtra(EXTRA_NAME, name);
+        intent.putExtra(EXTRA_EMAIL, email);
+        intent.putExtra(EXTRA_PRIORITY, priority);
+        startActivity(intent);
     }
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
